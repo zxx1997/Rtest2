463de097c103e86084858ed0518efc0294082474
zxx <369579262@qq.com>
2017-09-27 13:35:06 +0800

    Initial commit

---
+++ app/src/main/java/com/example/admin/rtest2/DIYView/DIYcircle.java
@@ -0,0 +1,30 @@
+package com.example.admin.rtest2.DIYView;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by admin on 2017/09/27/027.
+ */
+
+public class DIYcircle extends View{
+
+    public DIYcircle(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+    }
+}
